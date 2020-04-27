b097ac4ab04f1d809d988179635fe2e716e200d2
Tkouk <dai19022@uom.edu.gr>
2020-04-27 11:40:27 +0300

    Changed number of loops

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
-		for(int i=0; i<5; i++)
+		for(int i=0; i<10; i++)
 			System.out.println(i);
 		System.out.println("Another printout");
 		System.out.println("and Another printout");
